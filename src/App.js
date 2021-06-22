dad2092cefbea7d285db3f5723035b0318ef459d
Pururaj Singh Rajawat <pururajsrajawwat@gmail.com>
2021-06-22 13:02:00 +0530

    Frontend And API

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeScreen from './components/HomeScreen';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_dVdKzwEwGFTaR1lFdCxibEp-mzWoz5w",
+    authDomain: "netflix-clone-bfac4.firebaseapp.com",
+    projectId: "netflix-clone-bfac4",
+    storageBucket: "netflix-clone-bfac4.appspot.com",
+    messagingSenderId: "404149998512",
+    appId: "1:404149998512:web:d572a3d1859ccbd217bc75"
+};
