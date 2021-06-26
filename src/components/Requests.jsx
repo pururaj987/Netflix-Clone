cb88aad9c6533eab4d1489ce096df36bc088120f
Pururaj Singh Rajawat <pururajsrajawwat@gmail.com>
2021-06-26 18:12:20 +0530

    Updated the api key

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,5 +1,5 @@
-import api from "./api_key";
-const API_KEY = api.API_KEY;
+require("dotenv").config();
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
     fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-US` , 
